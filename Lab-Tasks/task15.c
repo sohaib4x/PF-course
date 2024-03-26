ade2ecbf7982ed715b2ef5a13054b85fd8d6ffdd
Sohaib Ahmad <145012326+sohaib4x@users.noreply.github.com>
2024-03-26 17:37:57 +0500

    Create task15.c

---
+++ Lab-Tasks/task15.c
@@ -0,0 +1,30 @@
+/* 
+Question>> Write a program which takes number as input and print stars in that
+format. (Use only while loop, don’t use nested for or while loop)*/
+
+
+#include <stdio.h>
+int main() 
+{                                                                                                     
+    int num;
+
+    printf("Enter the value for stars: "); // taking input
+    scanf("%d", &num);
+
+   int i = 1; //initializing i = 1
+
+   while (num > 0) { // using while loop 
+        if (num>=i){ // condition 
+        printf("*"); 
+        i++;
+        }
+        else {
+              // condition for next line
+            printf ("\n");
+            num--;
+            i = 1;
+        }
+        
+    }
+    return 0;
+}
