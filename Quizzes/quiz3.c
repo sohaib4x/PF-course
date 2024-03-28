be38d60b1af0bee2cb4fa8ff1511b7f220a35a13
Sohaib Ahmad <145012326+sohaib4x@users.noreply.github.com>
2024-03-29 01:11:45 +0500

    Update quiz3.c

--- Quizzes/quiz3.c
+++ Quizzes/quiz3.c
@@ -1,4 +1,28 @@
+/*Write a program that takes an integer from the user and finds if it is
+a palindrome, which mean the number is equal to its reverse
+
+• You should ONLY use a for loop
+
+• You SHOULD NOT use relational/logical operator in your program
+
+• Your program MUST NOT exceed 15 lines starting from int main to return 0*/
+
 #include <stdio.h>
-int main(){
-  printf("heloo");
+
+int main() {
+int num = 0,num2,remainder = 0,reverse = 0;
+
+printf("Enter any interger : ");
+scanf("%d",&num2);
+
+for(num = num2; num; num = num/10){
+    remainder = num%10;
+    reverse = reverse * 10 + remainder;
+}
+if(num2-reverse)
+printf("\nThe number is not a palindrome.\n");
+
+else
+printf("\nThe number is a Palindrome.\n");
+     
 }
