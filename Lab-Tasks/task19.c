d81b48bb47ba5af16caf9c9dba56301d2a8bf8c6
Sohaib Ahmad <145012326+sohaib4x@users.noreply.github.com>
2024-04-22 20:26:28 +0500

    Update task19.c

--- Lab-Tasks/task19.c
+++ Lab-Tasks/task19.c
@@ -1,4 +1,27 @@
+/*pattern using recursion*/
 #include <stdio.h>
-int main(){
-  printf("hello world");
+void printPattern(int,int);
+
+int main() {
+int rows;
+
+printf("Enter the number of rows for the pattern: ");
+scanf("%d", &rows);
+
+printPattern(rows, rows);
+
+return 0;
+}
+
+void printPattern(int rows, int cols) {
+
+if (rows == 0)
+return;
+
+for (int i = 0; i < cols; i++) {
+printf("*");
+}
+printf("\n");
+
+printPattern(rows - 1, cols);
 }
